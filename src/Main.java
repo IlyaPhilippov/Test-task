1ad0e5e76e1080fd53ff6e19b511b9de11ad30ff
Илья <ilya.philippov.ip@gmail.com>
2024-12-03 17:53:24 +0300

    new

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(calc("1 / 2"));
+
+    }
+    public static String calc(String input) {
+        int result = 0;
+        try {
+        String[] parts = input.split(" ");
+        if (parts.length > 3) throw new IllegalArgumentException();
+        int a = Integer.parseInt(parts[0]);
+        int b =Integer.parseInt(parts[2]);
+
+            if (a > 10 || b > 10 ) throw new NumberFormatException();
+
+            if (parts[1].matches("[+*/-]")) {
+                    switch (parts[1]) {
+                    case "+":
+                        result = a + b;
+                        break;
+                    case "-":
+                        result = a - b;
+                        break;
+                    case "*":
+                        result = a * b;
+                        break;
+                    case "/":
+                        result = a / b;
+                        break;
+                    }
+                } else throw new IllegalArgumentException();
+
+        }catch (NumberFormatException e){
+            throw new NumberFormatException();
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException();
+        }catch (IndexOutOfBoundsException e){
+           throw new IndexOutOfBoundsException();
+        }
+        return Integer.toString(result);
+    }
+}
